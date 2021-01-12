dbde27c043cafbc28bf26174e44f5a37c11eb0cf
NilsonKr <nuotrok444@gmail.com>
2021-01-12 16:07:20 -0500

    Addin request and fetch to bring the data

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,20 @@
-const  home = () => {
+import getData from '../utils/getData.js'
+
+
+
+
+const  home = async () => {
+    const data = await getData()
     const view = `
         <section class="home">
+            ${data.results.map(char => `
             <article class="home-card">
-                <a href="#/1/">
-                    <img src="route" alt="Name">
-                    <h2>Name</h2>
+                <a href="#/${char.id}/">
+                    <img src="${char.image}" alt="${char.name}">
+                    <h2>${char.name}</h2>
                 </a>
             </article>
+            `)}
         </section>
     `;
     return view
